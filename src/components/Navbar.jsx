378af2b835c6195f04d412dcd44aa41b8ee406fc
Tommy <tommy@brik.id>
2024-06-09 01:05:57 +0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Tommy Lie | Portfolio</h1>
+      </div>
+      <div>
+        <Link>About</Link>
+        <Link>Work</Link>
+        <Link>Contact</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
