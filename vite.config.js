0aa542b680ffa4a42b0410504085be274200fa89
itsTomLie <itsTomLie@gmail.com>
2024-09-19 23:17:12 +0800

    Test fix buffer not defined

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
-// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-
-import nodePolyfills from "vite-plugin-node-stdlib-browser";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
